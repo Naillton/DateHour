0b8916055aae455b7ce9e052df9e8a75b2967e43
Nailton Junior <51039019+Naillton@users.noreply.github.com>
2023-05-30 15:29:57 -0300

    trabalhando com datas

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@
 // O calendar e o date foram descontinuados pois nao tinham muitas opcoes de tratamento
 import java.time.Clock;
 import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class Main {
   public static void main(String[] args) {
     // usamos aqui o Clock da biblioteca time onde usamos o systemDefaultZone
@@ -14,5 +16,20 @@ public class Main {
     // com o LocalDateTime passamos o now() onde temos o resultado da hora e da data sem fuso horario
     LocalDateTime hour2 = LocalDateTime.now();
     System.out.println(hour2);
+    diaDeAcad();
+  }
+
+  private static void diaDeAcad() {
+    LocalDateTime work = LocalDateTime.now();
+    String day = work.getDayOfWeek().toString();
+    if (Objects.equals(day, "MONDAY")) {
+      System.out.println(day+" é dia de ir para a academia");
+    } else if (Objects.equals(day, "WEDNESDAY")) {
+      System.out.println(day+" é dia de ir para a academia");
+    } else if (Objects.equals(day, "FRIDAY")) {
+      System.out.println(day+" é dia de ir para a academia");
+    } else {
+      System.out.println("Hoje não é dia de academia");
+    }
   }
 }
